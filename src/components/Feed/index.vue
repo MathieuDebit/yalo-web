cfb9f087ccc054862f776ceaebea63edd7862801
Mathieu Débit <mathieu.debit@gmail.com>
2016-10-01 01:45:43 +0200

    feat(feed): style messages in feed

--- src/components/Feed/index.vue
+++ src/components/Feed/index.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="yalo-feed">
-    feed
+    <ul class="yalo-feed-messages-list">
+      <li class="yalo-feed-message-item">
+        <div class="yalo-feed-message-item-avatar"></div>
+
+        <div class="yalo-feed-message-item-wrapper">
+          <p class="yalo-feed-message-item-time">12:43</p>
+
+          <div class="yalo-feed-message-item-content">
+            yalo
+          </div>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
